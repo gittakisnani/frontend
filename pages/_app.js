aba6c8de9fb34ea9b50a434a2c5b53704e53e1b3
Hamza1176 <119422584+salmaoujanafront@users.noreply.github.com>
2022-12-12 23:45:55 +0600

    cookies policy added and search console verified

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import "../styles/style.css";
 import "../styles/legalnotice.css";
 import Head from "next/head";
+import CookieConsent from "react-cookie-consent";
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -13,6 +14,7 @@ function MyApp({ Component, pageProps }) {
         />
       </Head>
       <Component {...pageProps} />
+      <CookieConsent buttonStyle={{ backgroundColor: "#008039", fontSize: "16px",color:"#fff" }} buttonText="Accept">This website uses cookies to enhance the user experience.</CookieConsent>
     </>
   );
 }
