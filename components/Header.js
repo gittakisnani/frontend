2ed11397ecde9a6546b0eca1fe49db4ca1c1c240
Hamza1176 <119422584+salmaoujanafront@users.noreply.github.com>
2022-12-12 18:00:14 +0600

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -20,10 +20,10 @@ const Header = () => {
         </div>
         <div>
           <div className={navActive ? "links active" : "links"}>
-            <Link href="/" className="active">
+            <Link href="/" style="color:var(--secoundary)">
               HOME
             </Link>
-            <Link href="/legalnotice">legal notice contest</Link>
+            <Link href="/legalnotice" style="color:var(--secoundary);">legal notice contest</Link>
           </div>
         </div>
         <div className="outerdd">
