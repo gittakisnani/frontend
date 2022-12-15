ec18ec8b2135c380981140bc40b853be30469eec
Hamza1176 <119422584+salmaoujanafront@users.noreply.github.com>
2022-12-15 13:43:09 +0600

    Update changeprizestatus.js

--- pages/employee/changeprizestatus.js
+++ pages/employee/changeprizestatus.js
@@ -7,9 +7,23 @@ import { BiEditAlt } from 'react-icons/bi';
 
 
 const ChangePrizeSTatus = () => {
+  const [users, setUsers] = useState([]);
+  const getUserList = async () => {
+    await axios
+      .get("https://dsp-archiwebo21-ss-da-om-en.fr/api/v1/contest-participents")
+      .then((response) => setUsers(response.data));
+  };
+
+  const updateParticipent = async (userId,ticketId) =>{
+    await axios.put("https://dsp-archiwebo21-ss-da-om-en.fr/api/v1/update-participents",{userId,ticketId});
+    getUserList()
+  }
+  useEffect(() => {
+getUserList()
+  },[])
     return ( 
         <div>
-      <SideMenu />
+      {/* <SideMenu /> */}
       <div className="main-wrap">
         <div className="wrapSection">
           <div className="block active">
@@ -26,7 +40,20 @@ const ChangePrizeSTatus = () => {
                 </div>
                 
                 </div>
-
+                {/* _id
+6399fc75e03ef26841efca38
+userId
+"639411b583b9e3729935e54d"
+ticketId
+"0132788243"
+contestId
+"6399f91be03ef26841d8e69d"
+prize
+"signature tea"
+prizeStatus
+"Pending"
+mainPrizeResult
+"Loose" */}
               <div className="table">
                 <table>
                   <tbody>
@@ -37,13 +64,19 @@ const ChangePrizeSTatus = () => {
                       <th>Prix</th>
                       <th>Statut du prix</th>
                     </tr>
-                    <tr>
-                        <td>eeee</td>
-                        <td>eeee</td>
-                        <td>eeee</td>
-                        <td>eeee</td>
-                        <td><center><div className="smalledit"><button><BiEditAlt/></button></div></center></td>
-                      </tr>
+                    {users?.filter((item) => item.prize !== "").map((v,key) => (
+ <tr key={key}>
+ <td>{v.name}</td>
+ <td>{v.email}</td>
+ <td>{v.ticketId}</td>
+ <td>{v.prize}</td>
+ <td>
+  {v.prizeStatus != "Delivered" ? <center><button onClick={() => updateParticipent(v.userId,v.ticketId)}>Delivered</button></center> : v.prizeStatus}
+  
+  </td>
+</tr>
+                    ))}
+                   
 
                     {/* {!isSearch? users.map((users) => (
                       <tr key={users.id}>
